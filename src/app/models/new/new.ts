ef2832eff2a282310f472b910a45496a4933ca86
erikbg7 <erik.bg7@gmail.com>
2019-06-10 22:03:08 +0200

    Last epic news section added in home view (epic news model)

---
+++ src/app/models/new/new.ts
@@ -0,0 +1,16 @@
+export class EpicNew {
+
+  title: string;
+  body: string;
+  image: string;
+  time: string;
+  meta: string;
+
+  constructor(title: string, body: string, image: string, time: string, meta: string) {
+    this.title = title;
+    this.body = body;
+    this.image = image;
+    this.time = time;
+    this.meta = meta;
+  }
+}
